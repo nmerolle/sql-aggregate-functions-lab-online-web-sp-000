a594c8d6633ee61edb201b611d506332652ca1fe
Nicholas Merolle <nmerolle11@hotmail.com>
2021-01-02 22:56:59 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -9,8 +9,8 @@ def lowest_student_gpa
 end
 
 def average_student_gpa
-  
-end"SELECT MAX(gpa) FROM students"
+  "SELECT MAX(gpa) FROM students"
+end"SELECT MAX(gpa) FROM students
 
 def total_tardies_for_all_students
 end
